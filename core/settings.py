269abd29d14098de8c3ce272bf23521aef812058
AmirHossein Shirazi <ashzih2@gmail.com>
2024-11-30 23:10:31 +0330

    questions and comp debug

--- core/settings.py
+++ core/settings.py
@@ -128,7 +128,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 handler404 = 'landing.views.c404'
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
