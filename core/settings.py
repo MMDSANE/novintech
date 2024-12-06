316198903041b497a3167885118a117097486b40
MohammadHossein Maharlouei <hosseinmaharlooie@gmail.com>
2024-12-06 16:58:44 +0330

    Launch version V 1.0.0

--- core/settings.py
+++ core/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+    
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -123,6 +125,7 @@ STATIC_ROOT=os.path.join(BASE_DIR, "staticfiles")
 # تنظیمات دایرکتوری رسانه‌ها (media files)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
